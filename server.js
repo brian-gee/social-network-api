4d01418b265e0b06fc7bf7c2e0048f58ea776ea5
Brian Gee <briangeertsma@gmail.com>
2022-10-06 22:19:57 -0400

    added mongodb connection using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

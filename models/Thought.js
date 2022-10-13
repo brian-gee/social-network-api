cd99f75c3c9c482547f41d72647d7dccc5b3be27
Brian Gee <briangeertsma@gmail.com>
2022-10-13 17:35:41 -0400

    able to pull from thoughts now

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,29 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require ('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+        },
+        reactionBody : {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+)
+
 const ThoughtSchema = new Schema(
     {
         thoughtText:{
@@ -34,36 +57,9 @@ const ThoughtSchema = new Schema(
 );
 
 ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.reduce(
-        (total, reaction) => total + reaction.length + 1,
-        0
-    );
+    return this.reactions.length;
 })
 
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-        },
-        reactionBody : {
-            type: String,
-            required: true,
-            maxLength: 280
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt:{
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
-    },
-)
-
 const Thought = model('Thought', ThoughtSchema);
-const Reaction = model('Reaction', ReactionSchema);
 
-module.exports = { Thought, Reaction};
+module.exports = Thought;
